7e5a8e58e0a5bd5c79bd45c2a11f4eaf5e271d97
Vincent Rupp Jr <vincent@vaerek.net>
2016-02-16 20:27:56 -0800

    Map polyfeatures

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -10,15 +10,16 @@ function [X_poly] = polyFeatures(X, p)
 X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+% Instructions: Given a vector X, return a matrix X_poly where the p-th
 %               column of X contains the values of X to the p-th power.
 %
-% 
+%
 
+X_poly = [];
 
-
-
-
+for i = 1:p
+  X_poly = [X_poly (X .^ i)];
+end
 
 % =========================================================================
 
