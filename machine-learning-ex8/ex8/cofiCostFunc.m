5e571e8bc71b00ccd13ccb4e4aaed12473588080
Vincent Rupp Jr <vincent@vaerek.net>
2016-05-07 12:42:32 -0700

    Complete ex8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,19 +41,13 @@ Theta_grad = zeros(size(Theta));
 %
 
 J = sum(sum((((X * Theta') - Y) .^2) .* R)) / 2;
+X_grad = (((X * Theta') - Y) .* R) * Theta;
+Theta_grad = (((X * Theta') - Y) .* R)' * X;
 
 
-
-
-
-
-
-
-
-
-
-
-
+J = J + (sum(sum(Theta .^ 2)) + sum(sum(X .^ 2))) * (lambda / 2);
+X_grad = X_grad + (lambda .* X);
+Theta_grad = Theta_grad + (lambda .* Theta);
 
 % =============================================================
 
